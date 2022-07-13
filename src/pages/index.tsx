fd8ed4a525a15157e8763509fdda1932fce473ff
Patric de Paiva Fonseca <patric.ccomp@gmail.com>
2022-07-12 23:19:28 -0300

    Feat: Início do visual

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import { GetStaticProps } from 'next';
 
 import { getPrismicClient } from '../services/prismic';
 
+import { FiCalendar, FiUser } from 'react-icons/fi';
+
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
 
@@ -24,9 +26,61 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ postsPagination }: HomeProps) {
+  return (
+    <>
+      <div className={styles.container}>
+        <ul className={styles.postsList}>
+          <li className={styles.post}>
+            <h1 className={styles.postTitle}>Como utilizar hooks</h1>
+            <p className={styles.postDescription}>
+              Pensando em sincronização em vez de ciclos de vida.
+            </p>
+            <p className={styles.postInfo}>
+              <div className={styles.postCreatedAt}>
+                <FiCalendar color="#BBBBBB" style={{ marginRight: '5px' }} />{' '}
+                15Mar 2021
+              </div>
+              <div className={styles.author}>
+                <FiUser color="#BBBBBB" style={{ marginRight: '5px' }} />
+                Joseph Oliveira
+              </div>
+            </p>
+          </li>
+
+          <li className={styles.post}>
+            <h1 className={styles.postTitle}>Criando um app CRA do zero</h1>
+            <p className={styles.postDescription}>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App
+            </p>
+            <p className={styles.postInfo}>
+              <div className={styles.postCreatedAt}>
+                <FiCalendar color="#BBBBBB" style={{ marginRight: '5px' }} />
+                <span>15Mar 2021</span>
+              </div>
+              <div className={styles.author}>
+                <FiUser color="#BBBBBB" style={{ marginRight: '5px' }} />
+                <span>Danilo Vieira</span>
+              </div>
+            </p>
+          </li>
+          <a className={styles.buttonLoadPosts} href="">
+            Carregar mais posts
+          </a>
+        </ul>
+        {/* <ul>
+          {postsPagination?.results.map((post: Post) => (
+            <>
+              <h1 key={post.uid}>{post.data.title}</h1>
+              <p>{post.data.subtitle}</p>
+            </>
+          ))}
+        </ul> */}
+      </div>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
